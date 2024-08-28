4926b20a4afc306ef5b32e879caf403b29c0ed51
Htet Aung Khant <bob.h@bettercast.io>
2024-08-28 13:49:46 +0630

    First Commit Yayyy

---
+++ hello.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+console.log("ဆိုင်ကယ်တွေကိုရှောင်စီး")
+console.log("ရှီးလီးလီးလီးလီးလီ")
+
+// untracked files
+// trakced files -> git add hello.txt
+// commit -> git commit 
+// push -> git push origin branch
+// git push origin main
