002e460099074df8f7a77ab76d1be693b984b150
Htet Aung Khant <bob.h@bettercast.io>
2024-08-28 14:05:12 +0630

    Second Commit
    Forgot some lines lol

--- hello.js
+++ hello.js
@@ -2,6 +2,8 @@ console.log("Hello World");
 console.log("ဆိုင်ကယ်တွေကိုရှောင်စီး")
 console.log("ရှီးလီးလီးလီးလီးလီ")
 
+console.log("Fuck, i forgot some codes. HEHE")
+
 // untracked files
 // trakced files -> git add hello.txt
 // commit -> git commit 
